65fbc2bf9c853096476108bae9e0a3ff1254313e
Gerd Heber <gheber@hdfgroup.org>
2025-02-05 17:11:17 -0600

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,9 +6,10 @@ curl -O https://beta.quicklisp.org/quicklisp.lisp;
 # The installation script is expecting the user to hit the <RETURN> key;
 # as well, the @@texinfo:@opt{@@--batch@@texinfo:}@@ expects an EOF marker.
 # The use of ~echo~ seems to satisfy both requirements.
-echo | sbcl --script ./.devcontainer/basics.lisp
+echo | sbcl --script ./.devcontainer/basics.lisp;
 rm quicklisp.lisp;
-ln -s /workspaces/kenzo/kenzo.asd ~/.local/share/common-lisp/source/kenzo.asd
-ln -s /workspaces/kenzo/kenzo-test.asd ~/.local/share/common-lisp/source/kenzo-test.asd
+mkdir -p /root/.local/share/common-lisp/source;
+ln -s /workspaces/kenzo/kenzo.asd /root/.local/share/common-lisp/source/kenzo.asd;
+ln -s /workspaces/kenzo/kenzo-test.asd /root/.local/share/common-lisp/source/kenzo-test.asd;
 echo "Done installing.";
 } || { echo "Quicklisp is already installed."; }
